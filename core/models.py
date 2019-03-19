94ad0502c61683fb3140d65012204e7853f04a75
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-19 13:28:19 -0400

    made Post model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import datetime
+from django.utils.text import slugify
+from django.urls import reverse
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    url = models.URLField(max_length=255)
+    posted_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-posted_on']
+    
+    def save(self, *args, **kwargs):
+        self.set_slug()
+        super().save(*args, **kwargs)
+    
+    def set_slug(self):
+        if self.slug:
+            return
+        
+        base_slug = slugify(self.title)
+        slug = base_slug
+        n = 0
+
+        while Post.objects.filter(slug=slug).count():
+            n += 1
+            slug = base_slug + "-" + str(n)
+        
+        self.slug = slug
+    
+    def __str__(self):
+        return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', args=[str(self.slug)])
