f3111e4c3742ccded17edafd9415fd2955df679b
AlanaKaimi <ajbryce@live.com>
2019-03-20 14:53:53 -0400

    plugging along...

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('post/<int:pk>', views.post_detail_view, name='post_detail')
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from core.models import Post, Comment, Vote
 from django.views.generic import View
+from django.views import generic
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    context = {
 
+    posts = Post.objects.all()
+    context = {
+        
     }
 
     return render(request, 'index.html', context=context)
+
+# class PostDetailView(generic.DetailView):
+#     model = Post
+    
+    
+    
+# def post_detail_view(request, post_pk):
+#     posts = get_object_or_404(Post, pk=post_pk)
+
+#     context = {
+#         'posts': posts,
+#     }
+
+#     return render(request, 'post_detail', context=context)
+    
+
