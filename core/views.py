dae5be5330f944b6851d8200e84289a1fe062975
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-20 14:03:26 -0400

    added comments from Comment and votes from Vote to index view

--- core/views.py
+++ core/views.py
@@ -5,7 +5,11 @@ from django.views.generic import View
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
+    comments = Comment.objects.all()
+    votes = Vote.objects.all()
     context = {
         'posts': posts,
+        'comments': comment,
+        'votes': votes,
     }
     return render(request, 'index.html', context=context)
