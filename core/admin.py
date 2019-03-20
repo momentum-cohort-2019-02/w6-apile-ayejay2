a078587afdb9f5724b575d65d741227007cdaae7
AlanaKaimi <ajbryce@live.com>
2019-03-20 09:52:26 -0400

    Added 'text' to CommentAdmin display

--- core/admin.py
+++ core/admin.py
@@ -15,5 +15,6 @@ class PostAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     list_display = (
         'post', 
-        'created_at'
+        'created_at',
+        'text',
         )
