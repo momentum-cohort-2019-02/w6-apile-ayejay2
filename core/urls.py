f9d84369e80f30ae4d4952362ff311b36235a76f
AlanaKaimi <ajbryce@live.com>
2019-03-21 09:32:42 -0400

    working on comment, trouble getting comment box to show.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+# from core import view as core_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail')
+    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
+    # path('post/<slug:slug_id>', views.create_comment, name='post-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,12 @@ from django.views.generic import View
 from django.views import generic
 from django.http import HttpResponseRedirect
 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.http import require_http_methods
+
+from .forms import CommentForm
+
 # Create your views here.
 def index(request):
 
@@ -19,16 +25,20 @@ def index(request):
 
 class PostDetailView(generic.DetailView):
     model = Post
-    
-    
-    
-# def post_detail_view(request, post_pk):
-#     posts = get_object_or_404(Post, pk=post_pk)
 
-#     context = {
-#         'posts': posts,
-#     }
+### TODO: in production --------------------->   
 
-#     return render(request, 'post-detail', context=context)
-    
+def create_comment(request):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.posted_by = request.user
+            comment.created_at = timezone.now()
+            comment.save()
+            return redirect('post-detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'post_detail.html', {'form': form})
 
+### TODO: In Production ------------------------------>
