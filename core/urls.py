40a6b5e560b23cbcfe19a6430b9916d8256158af
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-21 22:17:56 -0400

    fixed misspelling of paht to path

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<slug:slug>/vote/', views.post_vote_view, name="vote_posts"),
-    paht('post/new/', views.post_new, name="post_new"),
+    path('post/new/', views.post_new, name="post_new"),
 ]
